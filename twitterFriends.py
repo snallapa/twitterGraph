16a70dc75428331ba955a860ae2151afe29a0708
Sahith Nallapareddy <sahith.reddy@gmail.com>
2017-10-07 21:41:44 -0400

    backend for twitter graph

---
+++ twitterFriends.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import twitter as twitterClient
+import json
+from flask.json import jsonify
+
+with open('twitterkeys.json') as data_file:    
+    data = json.load(data_file)
+
+twitter = twitterClient.Api(consumer_key=data["consumer_key"],
+                  consumer_secret=data["consumer_secret"],
+                  access_token_key=data["access_token_key"],
+                  access_token_secret=data["access_token_secret"],
+                  sleep_on_rate_limit=False)
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+	return "hello worlds"
+
+@app.route("/friends")
+@app.route("/friends/<userId>")
+def getFriends(userId=None):
+	return jsonify(list(map(lambda user: user.AsDict(),twitter.GetFriends(userId))))
